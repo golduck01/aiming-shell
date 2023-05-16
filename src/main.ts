c65c50ec909f650d58f169723788e0b1b378080a
lin <1152653890@qq.com>
2023-05-16 17:51:22 +0800

    初始化

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,18 @@
-import HandleThemes from "./lib/HandleThemes";
+import { Command } from "commander";
 
-const handles = new HandleThemes();
-handles.getFolderFiles("/Users/likai/Desktop/测试文件夹");
+const program = new Command();
+
+program.name("aims").description("一些常用的shell使用").version("0.1.0");
+
+program
+  .command("split")
+  .description("Split a string into substrings and display as an array")
+  .argument("<string>", "string to split")
+  .option("--first", "display just the first substring")
+  .option("-s, --separator <char>", "separator character", ",")
+  .action((str, options) => {
+    const limit = options.first ? 1 : undefined;
+    console.warn(str.split(options.separator, limit));
+  });
+
+program.parse();
