b4e9c7b9b5bdac87b1e882ee0f4fdadbb678910d
lin <1152653890@qq.com>
2023-05-16 17:35:42 +0800

    初始化项目

---
+++ .eslintrc.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ["airbnb-typescript-prettier"],
+  rules: {
+    "class-methods-use-this": "off",
+    "no-console": ["error", { allow: ["warn", "error"] }],
+  },
+};
---
+++ src/main.ts
@@ -0,0 +1,4 @@
+import HandleThemes from "./lib/HandleThemes";
+
+const handles = new HandleThemes();
+handles.getFolderFiles("/Users/likai/Desktop/测试文件夹");
