ba58698307e24d01745e5efa00fc0ad6508bea20
lin <1152653890@qq.com>
2023-05-23 18:03:10 +0800

    初始化

--- src/service/index.ts
+++ src/service/index.ts
@@ -1,10 +1,7 @@
 import sqlite3 from "sqlite3";
 import { open } from "sqlite";
+import dayjs from "dayjs";
 
-// const database = new sqlite3.Database("mydatebase.db", function (err) {
-//   console.log(err);
-//   console.log(database);
-// });
 async function createDB() {
   const db = await open({
     filename: "database.db",
@@ -12,13 +9,35 @@ async function createDB() {
   });
   try {
     console.log(db);
-    await db.exec("CREATE TABLE tbl (col TEXT)");
-    await db.exec('INSERT INTO tbl VALUES ("test")');
-    const tab1 = await db.get("select * from tbl");
+    await db.exec(`
+    CREATE TABLE record_group
+    (id INTEGER PRIMARY KEY   AUTOINCREMENT),
+    (name text),
+    (created_at text),
+    (description text),
+    (records text),
+    (alias text),
+    (description text),
+    `);
+    const records = {
+      statement: "",
+      description: "",
+    };
+    await db.exec(
+      `INSERT INTO record_group VALUES (
+        "nginx",
+        "${dayjs().format("YYYY-MM-DD HH:mm:ss")}",
+        "nginx 命令组",
+        "${records}"
+        )`,
+      {}
+    );
+    await db.exec("delete from tbl");
+    const tab1 = await db.get("select * from record_group");
     console.log(tab1);
   } catch (e) {
     const error = e as Error;
-    console.log(error.message);
+    console.log(error);
   }
 }
 sqlite3.verbose();
